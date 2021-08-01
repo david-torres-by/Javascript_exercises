8508d3334bd1095afef94564f8c9f2b249234991
david-torres-by <davidtorresc8@gmail.com>
2021-08-01 09:45:01 -0500

    changes

--- Slicing Strings/tweet_exercise.js
+++
@@ -1,6 +0,0 @@
-// In here we're trying to create a tweet that is under 140 and if the user writes more, it will return 
-// the alert with the string cut.
-
-var paragraph = prompt("Compose your tweet!");
-var tweet_length = paragraph.slice(0,140);
-alert(tweet_length);
